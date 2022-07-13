746b02ac297bf95ca8b58d8b62d6db1207e12b75
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-13 09:35:31 +0200

    home view counter setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,23 @@
 <template>
 	<div class="home">
 		<h1>Home</h1>
+
+		<div>
+			<button class="btn">-</button>
+			<span class="counter">0</span>
+			<button class="btn">+</button>
+		</div>
 	</div>
 </template>
+
+<style>
+.home {
+	text-align: center;
+	padding: 2rem;
+}
+.btn,
+.counter {
+	font-size: 2rem;
+	padding: 2rem;
+}
+</style>
