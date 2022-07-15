b1e73ab9de0286319c8bf65f8e1d2355c47b4df9
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-15 13:41:48 +0200

    create single post links in PostsView

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,5 +1,16 @@
 <template>
 	<div class="posts">
 		<h1>Posts</h1>
+		<ul>
+			<li>
+				<RouterLink to="/post/id1">Post 1</RouterLink>
+			</li>
+			<li>
+				<RouterLink to="/post/id2">Post 2</RouterLink>
+			</li>
+			<li>
+				<RouterLink to="/post/id3">Post 3</RouterLink>
+			</li>
+		</ul>
 	</div>
 </template>
