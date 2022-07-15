0b9ccf642cce6f80f1a9477240d28b28756c0f91
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-15 14:06:24 +0200

    style PostDetail, create showPostId method

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -1,14 +1,34 @@
 <template>
 	<div class="post">
 		<h1>Post Detail</h1>
-		<p>Display the content of post with ID of {{ $route.params.id }} here !</p>
+		<p>
+			Display the content of post with ID of <strong>{{ $route.params.id }}</strong> here !
+		</p>
+		<hr />
+		<button @click="showPostId" class="btn">Show Post ID</button>
+		<hr />
 		<RouterLink to="/posts">&lt;&lt; Back</RouterLink>
 	</div>
 </template>
 
+<script setup>
+const showPostId = () => {
+	console.log('showPostId');
+};
+</script>
 <style scoped>
 .post {
 	text-align: center;
 	padding: 2rem;
+}
+.btn {
+	font-size: 1.5rem;
+	padding: 2rem;
+	margin-left: 1rem;
+	margin-right: 1rem;
+}
+hr {
+	margin-top: 2rem;
+	margin-bottom: 2rem;
 }
 </style>
