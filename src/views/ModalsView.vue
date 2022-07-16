949374f21171b9db14462521ef3753501be7d81b
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-16 14:20:16 +0200

    modal markup

--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -1,5 +1,33 @@
 <template>
 	<div class="modals">
 		<h1>Modals</h1>
+		<button class="btn">Show Modal</button>
+		<div class="modal">
+			<h1>This Is Our Modal</h1>
+			<h2>Oh This Is Amazing</h2>
+			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed at nostrum ipsam hic numquam! Animi impedit inventore qui minima voluptas error nisi fuga, nemo debitis, quibusdam, in corrupti eius porro facilis. Omnis cupiditate, error rem alias temporibus modi qui officia!</p>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quasi, earum minima quibusdam deleniti saepe ipsam accusamus recusandae at quos.</p>
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem quod, dolorem, odio unde maxime eius illo facere voluptatum repellat porro voluptate? Architecto, fugit! Consectetur officiis error facere eaque fugit cum, molestias tenetur voluptatum, itaque rem possimus deleniti placeat dicta exercitationem laudantium? Modi inventore quis, omnis dolorem illo natus explicabo nemo provident commodi, nobis pariatur amet, iure sunt dignissimos voluptatibus corrupti.</p>
+			<button class="btn">Hide Modal</button>
+		</div>
 	</div>
 </template>
+
+<style scoped>
+.modals {
+	text-align: center;
+	padding: 2rem;
+}
+.btn {
+	font-size: 1.5rem;
+	padding: 2rem;
+	margin-left: 1rem;
+	margin-right: 1rem;
+}
+.modal {
+	background-color: aquamarine;
+	border: 10px solid midnightblue;
+	border-radius: 15%;
+	padding: 2rem;
+}
+</style>
