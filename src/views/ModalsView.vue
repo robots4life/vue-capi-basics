2efc4bd08b7e44d584efc5946df5d85a95d07815
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-16 14:25:32 +0200

    hide modal

--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -1,14 +1,14 @@
 <template>
 	<div class="modals">
 		<h1>Modals</h1>
-		<button class="btn" @click="showModal = !showModal">Show Modal</button>
+		<button @click="showModal = !showModal" class="btn">Toggle Modal</button>
 		<div class="modal" v-if="showModal">
 			<h1>This Is Our Modal</h1>
 			<h2>Oh This Is Amazing</h2>
 			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed at nostrum ipsam hic numquam! Animi impedit inventore qui minima voluptas error nisi fuga, nemo debitis, quibusdam, in corrupti eius porro facilis. Omnis cupiditate, error rem alias temporibus modi qui officia!</p>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quasi, earum minima quibusdam deleniti saepe ipsam accusamus recusandae at quos.</p>
 			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem quod, dolorem, odio unde maxime eius illo facere voluptatum repellat porro voluptate? Architecto, fugit! Consectetur officiis error facere eaque fugit cum, molestias tenetur voluptatum, itaque rem possimus deleniti placeat dicta exercitationem laudantium? Modi inventore quis, omnis dolorem illo natus explicabo nemo provident commodi, nobis pariatur amet, iure sunt dignissimos voluptatibus corrupti.</p>
-			<button class="btn">Hide Modal</button>
+			<button @click="showModal = false" class="btn">Hide Modal</button>
 		</div>
 	</div>
 </template>
