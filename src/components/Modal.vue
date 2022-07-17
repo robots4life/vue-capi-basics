3c70867d9056faf42de6f6dcc0cd771e2335676d
robots4life <34193243+robots4life@users.noreply.github.com>
2022-07-17 04:32:42 +0200

    add slot to modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,10 +2,7 @@
 	<teleport to="body">
 		<div class="modal">
 			<h1>This Is Our Modal</h1>
-			<h2>Oh This Is Amazing</h2>
-			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed at nostrum ipsam hic numquam! Animi impedit inventore qui minima voluptas error nisi fuga, nemo debitis, quibusdam, in corrupti eius porro facilis. Omnis cupiditate, error rem alias temporibus modi qui officia!</p>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quasi, earum minima quibusdam deleniti saepe ipsam accusamus recusandae at quos.</p>
-			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem quod, dolorem, odio unde maxime eius illo facere voluptatum repellat porro voluptate? Architecto, fugit! Consectetur officiis error facere eaque fugit cum, molestias tenetur voluptatum, itaque rem possimus deleniti placeat dicta exercitationem laudantium? Modi inventore quis, omnis dolorem illo natus explicabo nemo provident commodi, nobis pariatur amet, iure sunt dignissimos voluptatibus corrupti.</p>
+			<slot />
 			<button class="btn">Hide Modal</button>
 		</div>
 	</teleport>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,7 +2,12 @@
 	<div class="modals">
 		<h1>Modals</h1>
 		<button @click="showModal = !showModal" class="btn">Toggle Modal</button>
-		<Modal v-if="showModal"></Modal>
+		<Modal v-if="showModal">
+			<h2>Oh This Is Amazing</h2>
+			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed at nostrum ipsam hic numquam! Animi impedit inventore qui minima voluptas error nisi fuga, nemo debitis, quibusdam, in corrupti eius porro facilis. Omnis cupiditate, error rem alias temporibus modi qui officia!</p>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quasi, earum minima quibusdam deleniti saepe ipsam accusamus recusandae at quos.</p>
+			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem quod, dolorem, odio unde maxime eius illo facere voluptatum repellat porro voluptate? Architecto, fugit! Consectetur officiis error facere eaque fugit cum, molestias tenetur voluptatum, itaque rem possimus deleniti placeat dicta exercitationem laudantium? Modi inventore quis, omnis dolorem illo natus explicabo nemo provident commodi, nobis pariatur amet, iure sunt dignissimos voluptatibus corrupti.</p>
+		</Modal>
 	</div>
 </template>
 
